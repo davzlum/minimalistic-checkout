be423b381496204650b8bb6f0cd7dc3f05dad6ec
David Vazquez <76447293+davzlum@users.noreply.github.com>
2021-11-28 23:18:05 +0100

    Set validation form and error messages

--- src/components/CardPaymentForm/index.jsx
+++ src/components/CardPaymentForm/index.jsx
@@ -1,28 +1,67 @@
+/* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable max-len */
 import React, { useState } from 'react';
 import CountriesDropdown from '../CountriesDropdown';
 import cvcIcon from '../../assets/cvcIcon.svg';
 
 const CardPaymentForm = function () {
-  const [state, setState] = useState({
-    number: '',
+  const validationsForm = (formulary) => {
+    const errorsMessage = {};
+    const emailRegex = /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/i;
+    const validateEmail = emailRegex.test(formulary.email);
+    if (!validateEmail) {
+      errorsMessage.email = '* Email incorrecto';
+    } else {
+      errorsMessage.email = '';
+    }
+    const cardRegex = /[0-9]{15,16}|(([0-9]{4}\s){3}[0-9]{3,4})/;
+    const validateCard = cardRegex.test(formulary.cardNumber);
+    if (!validateCard) {
+      errorsMessage.cardNumber = '* Número incorrecto';
+    } else {
+      errorsMessage.cardNumber = '';
+    }
+    const dateRegex = /(?:0[1-9]|1[0-2])\/[0-9]{2}/;
+    const validateDate = dateRegex.test(formulary.dataExpiration);
+    if (!validateDate) {
+      errorsMessage.dataExpiration = '* Fecha incorrecta';
+    } else {
+      errorsMessage.dataExpiration = '';
+    }
+    const cvcRegex = /^[0-9]{3,4}$/;
+    const validateCvc = cvcRegex.test(formulary.cvcNumber);
+    if (!validateCvc) {
+      errorsMessage.cvcNumber = '* Número CVC incorrecto';
+    } else {
+      errorsMessage.cvcNumber = '';
+    }
+    return errorsMessage;
+  };
+  const initialForm = {
+    email: '',
+    cardNumber: '',
+    dataExpiration: '',
+    cvcNumber: '',
     name: '',
-    expiry: '',
-    cvc: '',
-    focus: '',
-  });
-  const handleInputChange = (e) => {
-    setState({
-      ...state,
-      [e.target.name]: e.target.value,
+    country: '',
+    zip: '',
+  };
+  const [form, setForm] = useState(initialForm);
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({
+      ...form,
+      [name]: value,
     });
   };
-  const handleFocusChange = (e) => {
-    setState({
-      ...state,
-      focus: e.target.name,
-    });
+
+  const handleSubmit = (e) => {
+    handleChange(e);
+    setErrors(validationsForm(form));
   };
+
   return (
     <form>
       <div className="form-group">
@@ -33,7 +72,12 @@ const CardPaymentForm = function () {
             name="email"
             id="email"
             className="form-control"
+            onChange={handleChange}
+            required
           />
+          <div className="error-container">
+            {errors.email && <span>{errors.email}</span>}
+          </div>
         </label>
       </div>
       <div className="form-group">
@@ -44,12 +88,12 @@ const CardPaymentForm = function () {
               type="text"
               inputMode="numeric"
               placeholder="1234 1234 1234 1234"
-              name="number"
+              name="cardNumber"
               id="number"
-              maxLength="16"
+              maxLength="19"
               className="form-control top"
-              onChange={handleInputChange}
-              onFocus={handleFocusChange}
+              onChange={handleChange}
+              required
             />
             <div className="card-icons">
               <img src="https://js.stripe.com/v3/fingerprinted/img/visa-365725566f9578a9589553aa9296d178.svg" alt="" />
@@ -65,28 +109,33 @@ const CardPaymentForm = function () {
               <input
                 type="text"
                 inputMode="numeric"
-                name="expiry"
+                name="dataExpiration"
                 id="expiry"
-                maxLength="4"
+                maxLength="5"
                 className="form-control bottom-left"
                 placeholder="MM / YY"
-                onChange={handleInputChange}
-                onFocus={handleFocusChange}
+                onChange={handleChange}
+                required
               />
             </label>
+            <div className="error-container">
+              {errors.cardNumber && <span>{errors.cardNumber}</span>}
+              {errors.dataExpiration && <span>{errors.dataExpiration}</span>}
+              {errors.cvcNumber && <span>{errors.cvcNumber}</span>}
+            </div>
           </div>
           <div className="col-md-6">
             <label htmlFor="cvc">
               <input
                 type="text"
                 inputMode="numeric"
-                name="cvc"
+                name="cvcNumber"
                 id="cvc"
-                maxLength="4"
+                maxLength="3"
                 className="form-control bottom-right"
                 placeholder="CVC"
-                onChange={handleInputChange}
-                onFocus={handleFocusChange}
+                onChange={handleChange}
+                required
               />
               <div className="icon">
                 <img src={cvcIcon} alt="cvc" />
@@ -104,13 +153,13 @@ const CardPaymentForm = function () {
             id="name"
             maxLength="30"
             className="form-control"
-            onChange={handleInputChange}
-            onFocus={handleFocusChange}
+            onChange={handleChange}
+            required
           />
         </label>
       </div>
       <CountriesDropdown />
-      <button type="button" className="button-pay">Pay $899.00</button>
+      <input onClick={handleSubmit} type="submit" className="button-pay" value="Pay $899.00" />
     </form>
   );
 };
