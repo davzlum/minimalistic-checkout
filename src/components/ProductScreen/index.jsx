b9b51ea705c9666097f7f4c5d675ca2dde89bd55
David Vazquez <76447293+davzlum@users.noreply.github.com>
2021-11-28 18:35:11 +0100

    Edit product screen component

--- src/components/ProductScreen/index.jsx
+++ src/components/ProductScreen/index.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
-import CountriesDropdown from '../CountriesDropdown';
+import Footer from '../Footer';
 import GoBackButton from '../GoBackButton';
+import './style.scss';
 
 const ProductScreen = function () {
   return (
-    <>
+    <div className="product-screen">
       <GoBackButton />
-      <div>New Mac</div>
-      <div>$ 899.00</div>
-      <img src="https://i.ibb.co/9YxksKN/image-6.png" alt="product" />
-      <CountriesDropdown />
-    </>
+      <div className="info-text">
+        <div className="product-description">
+          <div>New Mac</div>
+          <div className="price">$ 899.00</div>
+        </div>
+        <img className="product-img" src="https://i.ibb.co/9YxksKN/image-6.png" alt="product" />
+      </div>
+      <Footer />
+    </div>
   );
 };
 
